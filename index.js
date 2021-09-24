533bd732ed41d64e5dcc2ab29d5f0872a39fa46b
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-24 03:37:30 -0400

    Adds function calls to index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP("99.245.165.63", (error, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned data:' , data);
 });
