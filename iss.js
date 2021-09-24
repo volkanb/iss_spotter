f919828c5db5fe6deffc7f3cde47da02fd75e12d
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-24 03:09:41 -0400

    Adds non 200 response status handling logic

--- iss.js
+++ iss.js
@@ -10,12 +10,20 @@ const request = require('request');
  const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
+    // error can be set if invalid domain, user is offline, etc.
     if (error) {
       callback(error, null);
-    } else {
-      const data = JSON.parse(body);
-      callback(null, data.ip);
+      return;
     }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // if we get here, all's well and we got the data
+    const data = JSON.parse(body);
+    callback(null, data.ip);
   });
   
 }
