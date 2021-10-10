e84c195d48dea653282724be40eceb60cc3fce3e
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-10-10 19:16:12 -0400

    Adds fetchCoordsByIP returning a promise

--- index2.js
+++ index2.js
@@ -1,3 +1,5 @@
-const { fetchMyIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
 
-fetchMyIP().then(body => console.log(body));
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(body => console.log(body));
--- iss_promised.js
+++ iss_promised.js
@@ -10,4 +10,15 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
-module.exports = { fetchMyIP };
+
+/* 
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
